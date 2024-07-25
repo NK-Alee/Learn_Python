c3c98c2bcc73ab39d7213ae1535e970c662ddc7c
Alee <91377195+NK-Alee@users.noreply.github.com>
2024-07-25 23:16:48 +0700

    Add files via upload

---
+++ Bài Số 31 Giới thiệu về Lập Trình Hướng đối tượng và cách vẽ sơ đồ lớp.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 20:47:18 2024
+
+@author: Alee
+Bài Số 31 Giới thiệu về Lập Trình Hướng đối tượng và cách vẽ sơ đồ lớp
+"""
+"""
+Bài tập: Quản lý sinh viên
+Một sinh viên ở trường đại học XYZ có các thông tin : Mã sinh viên, Họ và tên , ngày tháng năm sinh,
+quê quán và lớp học . Một lớp học thường có các thông tin như : Mã lớp học , tên lớp học , niên khóa.
+Ngày sinh bao gồm các thông tin là ngày, tháng, năm.
+
+Sinh viên có một số hành động như sau : Đăng ký môn học ( Nhận vào một mã môn học), thi. Một lớp
+hocc5 có các hành động như : in thông tin
+
+Hãy thiết kế sơ đồ lớp
+Thực Hành trên startuml 
+"""
